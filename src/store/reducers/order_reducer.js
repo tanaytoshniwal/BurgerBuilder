8e85fd48d0f5b21dedec8405039086c3c3383c4a
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2020-06-11 02:26:41 +0530

    bug fixed in reducer

--- src/store/reducers/order_reducer.js
+++ src/store/reducers/order_reducer.js
@@ -48,13 +48,13 @@ const fetchOrdersFail = (state, action) => {
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
-        case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess()
-        case actionTypes.PURCHASE_BURGER_FAIL: return purchaseBurgerFail()
-        case actionTypes.PURCHASE_BURGER_START: return purchaseBurgerStart()
-        case actionTypes.PURCHASE_INIT: return purchaseInit()
-        case actionTypes.FETCH_ORDERS_START: return fetchOrdersStart()
-        case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrdersSuccess()
-        case actionTypes.FETCH_ORDERS_FAIL: return fetchOrdersFail()
+        case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess(state, action)
+        case actionTypes.PURCHASE_BURGER_FAIL: return purchaseBurgerFail(state, action)
+        case actionTypes.PURCHASE_BURGER_START: return purchaseBurgerStart(state, action)
+        case actionTypes.PURCHASE_INIT: return purchaseInit(state, action)
+        case actionTypes.FETCH_ORDERS_START: return fetchOrdersStart(state, action)
+        case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrdersSuccess(state, action)
+        case actionTypes.FETCH_ORDERS_FAIL: return fetchOrdersFail(state, action)
         default: return state
     }
 }
